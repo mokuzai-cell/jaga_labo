63814951a2e9c54baaf4c0e1df527d9c71ddea6d
mokuzai-cell <7525109@ed.tus.ac.jp>
2025-11-22 14:14:30 +0900

    履歴消去ロジック変更

--- src/Tab_history.vue
+++ src/Tab_history.vue
@@ -6,9 +6,9 @@ import trash from './assets/trash.svg'
 import QRgenerator from './QRgenerator.vue'
 
 let orderHistory = defineModel<Order[]>()
-let showorder=ref<Order|null>(null);
-let isshowQR=ref<boolean>(false);
-let orderedPopRef=ref<HTMLDivElement|null>(null);
+let showorder = ref<Order | null>(null)
+let isshowQR = ref<boolean>(false)
+let orderedPopRef = ref<HTMLDivElement | null>(null)
 
 function order2Time(order: Order) {
   let t = new Date(order.time)
@@ -25,46 +25,72 @@ function order2Time(order: Order) {
 
   return formattedDate
 }
-function trashHandler(){
-    if(confirm("注文履歴をすべて削除しますか？\nこの操作は取り消せません")){
-        localStorage.removeItem("orderHistory");
-        alert("消去しました。");
-        orderHistory.value=[];
+function trashHandler() {
+  if (confirm('注文履歴をすべて削除しますか？\nこの操作は取り消せません')) {
+    localStorage.removeItem('orderHistory')
+    alert('消去しました。')
+    if (orderHistory.value) {
+      while (orderHistory.value.length > 0) {
+        orderHistory.value.pop()
+      }
     }
+  }
 }
-function showPopover(idx:number){
-    if(orderHistory.value && orderHistory.value[idx])showorder.value=orderHistory.value[idx]
-    isshowQR.value=false;
-    if(orderedPopRef.value)orderedPopRef.value.showPopover();
+function showPopover(idx: number) {
+  if (orderHistory.value && orderHistory.value[idx]) showorder.value = orderHistory.value[idx]
+  isshowQR.value = false
+  if (orderedPopRef.value) orderedPopRef.value.showPopover()
 }
 </script>
 
 <template>
-  <h2 style="text-align: center;">注文履歴</h2>
-  <div v-for="(order, idx) in orderHistory" class="historyWrapper" @click="()=>showPopover(idx)">
+  <h2 style="text-align: center">注文履歴</h2>
+  <div v-for="(order, idx) in orderHistory" class="historyWrapper" @click="() => showPopover(idx)">
     <div class="historymini">
-        <p class="ID">#{{ idx }}</p>
-        <p class="historyTime" :key="idx">注文日時：<span>{{ order2Time(order) }}</span></p>
+      <p class="ID">#{{ idx }}</p>
+      <p class="historyTime" :key="idx">
+        注文日時：<span>{{ order2Time(order) }}</span>
+      </p>
     </div>
     <div class="historymini">
-        <p class="historyType">大きさ：<span>{{ goods.types[order.type] }}</span></p>
-        <p class="historyCount">個数：<span>{{ order.count }}</span></p>
-    </div>  
+      <p class="historyType">
+        大きさ：<span>{{ goods.types[order.type] }}</span>
+      </p>
+      <p class="historyCount">
+        個数：<span>{{ order.count }}</span>
+      </p>
+    </div>
   </div>
-  <div style="height:200px"></div>
-  <img :src="trash" class="trashBtn" @click="trashHandler"></img>
+  <div style="height: 200px"></div>
+  <img :src="trash" class="trashBtn" @click="trashHandler" />
   <div id="orderedPopup" popover="auto" ref="orderedPopRef">
     <div v-if="showorder">
-        <QRgenerator v-if="isshowQR" :value="showorder?showorder.id:''" class="QR" style="width:200px;"/><br/>
-        <button @click="()=>{isshowQR=!isshowQR}">QRコードを{{ isshowQR?"非表示にする":"表示する" }}</button>
-        <p>{{ showorder.id }}</p>
-        <p>日時：{{ showorder.time }}</p>
-        <p>大きさ：{{ showorder.type }}</p>
-        <p>個数：{{ showorder.count }}</p>
-        <p>合計金額：{{ showorder.allMoney }}</p>
-        <button popovertarget="orderedPopup" popovertargetaction="hide" id="tojiru">閉じる</button>
+      <QRgenerator
+        v-if="isshowQR"
+        :value="showorder ? showorder.id : ''"
+        class="QR"
+        style="width: 200px"
+      /><br />
+      <button
+        @click="
+          () => {
+            isshowQR = !isshowQR
+          }
+        "
+      >
+        QRコードを{{ isshowQR ? '非表示にする' : '表示する' }}
+      </button>
+      <p>{{ showorder.id }}</p>
+      <p>日時：{{ showorder.time }}</p>
+      <p>大きさ：{{ showorder.type }}</p>
+      <p>個数：{{ showorder.count }}</p>
+      <p>合計金額：{{ showorder.allMoney }}</p>
+      <button popovertarget="orderedPopup" popovertargetaction="hide" id="tojiru">閉じる</button>
     </div>
   </div>
+  <p v-show="orderHistory && orderHistory.length == 0" style="text-align: center">
+    まだ注文履歴がありません。
+  </p>
 </template>
 <style scoped>
 .historyWrapper {
@@ -75,51 +101,53 @@ function showPopover(idx:number){
   border-left: solid 5px #5d627b;
   box-shadow: 0 3px 5px rgba(0, 0, 0, 0.22);
 }
-.historyWrapper:active{
-    box-shadow: none;
+.historyWrapper:active {
+  box-shadow: none;
 }
-.trashBtn{
-    width: 40px;
-    height: 40px;
-    position: absolute;
-    right: 0.3em;
-    top: 3em;
-    padding: 10px;
-    background-color: #e07474;
-    border-radius: 30px;
-    box-shadow:inset 0 2px 0 rgba(255,255,255,0.5), 0 2px 2px rgba(0, 0, 0, 0.19);
+.trashBtn {
+  width: 40px;
+  height: 40px;
+  position: absolute;
+  right: 0.3em;
+  top: 3em;
+  padding: 10px;
+  background-color: #e07474;
+  border-radius: 30px;
+  box-shadow:
+    inset 0 2px 0 rgba(255, 255, 255, 0.5),
+    0 2px 2px rgba(0, 0, 0, 0.19);
 }
-.trashBtn:active{
-    background-color: #e58888;
-    box-shadow: none;
+.trashBtn:active {
+  background-color: #e58888;
+  box-shadow: none;
 }
-.historyWrapper{
-    p{
-        span{
-            color:black;
-            font-size:1.1em;
-        }
+.historyWrapper {
+  p {
+    span {
+      color: black;
+      font-size: 1.1em;
     }
+  }
 }
-.historymini{
-    display:flex;
-    align-items: baseline;
-    justify-content: space-around;
+.historymini {
+  display: flex;
+  align-items: baseline;
+  justify-content: space-around;
 }
-.ID{
-    font-size:2em;
-    margin-right:2em;
+.ID {
+  font-size: 2em;
+  margin-right: 2em;
 }
-#orderedPopup{
-      width: 80%;
+#orderedPopup {
+  width: 80%;
   height: 75%;
   transform: translateY(-5%);
   text-align: center;
 }
-.QR{
-    width:200px;
+.QR {
+  width: 200px;
 }
-button{
+button {
   display: inline-block;
   padding: 0.5em 0.5em;
   text-decoration: none;
@@ -132,14 +160,13 @@ button{
   margin-top: 0.5em;
   border: none;
 }
-button:active{
+button:active {
   -webkit-transform: translateY(4px);
   transform: translateY(4px); /*下に動く*/
   box-shadow: 0px 0px 1px rgba(0, 0, 0, 0.2); /*影を小さく*/
   border-bottom: none;
 }
-#tojiru{
-    width: 80%;
-
+#tojiru {
+  width: 80%;
 }
 </style>
